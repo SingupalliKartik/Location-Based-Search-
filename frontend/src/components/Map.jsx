f5e38d4c5d1497302d281b236de91d5d3c6c8cb0
Singupalli Kartik <skartik1706@gmail.com>
2024-03-09 18:20:16 +0530

    Add @mui/icons-material package and update Map component

--- frontend/src/components/Map.jsx
+++ frontend/src/components/Map.jsx
@@ -4,7 +4,7 @@ import TextField from "@mui/material/TextField";
 import Slider from "@mui/material/Slider";
 import Button from "@mui/material/Button";
 import GoogleMapReact from "google-map-react";
-import LocationSearchingIcon from '@mui/icons-material/LocationSearching';
+import LocationSearchingIcon from "@mui/icons-material/LocationSearching";
 const Map = () => {
   const [latitude, setLatitude] = useState(0);
   const [longitude, setLongitude] = useState(0);
@@ -88,7 +88,19 @@ const Map = () => {
       </div>
     );
   };
-
+  let userloction = [[
+    { "id": "1", "name": "Location 1", "lat": "23.237541", "lng": "77.405549" },
+    { "id": "2", "name": "Location 2", "lat": "23.240698", "lng": "77.395453" },
+    { "id": "3", "name": "Location 3", "lat": "23.236874", "lng": "77.421120" },
+    { "id": "4", "name": "Location 4", "lat": "23.243567", "lng": "77.397823" },
+    { "id": "5", "name": "Location 5", "lat": "23.252890", "lng": "77.378740" },
+    { "id": "6", "name": "Location 6", "lat": "23.239640", "lng": "77.432620" },
+    { "id": "7", "name": "Location 7", "lat": "23.229382", "lng": "77.412512" },
+    { "id": "8", "name": "Location 8", "lat": "23.245191", "lng": "77.438478" },
+    { "id": "9", "name": "Location 9", "lat": "23.255556", "lng": "77.419581" },
+    { "id": "10", "name": "Location 10", "lat": "23.264987", "lng": "77.397469" }
+]
+];
   const map = () => {
     return (
       <div style={{ backgroundColor: "cyan", height: "80vh" }}>
@@ -99,9 +111,26 @@ const Map = () => {
             lng: 75.8577258,
           }}
           defaultZoom={14}
-      center={{lat: latitude, lng: longitude}}
+          center={{ lat: latitude, lng: longitude }}
         >
+
+          {/* {userloction.map((location) => {
+            return (
+              <LocationSearchingIcon
+                color="primary"
+                lat={location.lat}
+                lng={location.lng}/>)
+          })} */}
           {/* <AnyReactComponent lat={59.955413} lng={30.337844} text="My Marker" /> */}
+
+          <LocationSearchingIcon
+            color="primary"
+            lat={latitude}
+            lng={longitude}
+            text="My Marker"
+            fontSize="large"
+          />
+          
         </GoogleMapReact>
       </div>
     );
