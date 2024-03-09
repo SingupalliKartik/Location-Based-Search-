9f4cfa8aa0179791c67be8c15cfe5018d080950b
Vedansh Jain <vedansh.code@gmail.com>
2024-03-09 11:49:24 +0530

    Profile Completion Form Added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,13 +3,15 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Router from './components/Router'
+import Profile from './components/Profile'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <Router/>
+    {/* <Router/> */}
+    <Profile></Profile>
  </>
   )
 }
