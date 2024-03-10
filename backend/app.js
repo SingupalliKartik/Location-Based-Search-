2958085cb9ed664075245ed921709365641e2a0f
shreyash jain <sheryash123jain@gmail.com>
2024-03-10 18:50:47 +0530

    u

--- backend/app.js
+++ backend/app.js
@@ -21,7 +21,7 @@ try {
 }
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://virtualhackathon.vercel.app",
     methods: ["POST", "GET", "OPTIONS", "PATCH", "PUT", "DELETE"],
     credentials: true,
   })
@@ -29,7 +29,7 @@ app.use(
 
 
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.header("Access-Control-Allow-Origin", "https://virtualhackathon.vercel.app");
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
     "Access-Control-Allow-Headers",
