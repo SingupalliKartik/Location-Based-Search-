9ce906b7cc044a15c8b4757ce3fdaeded78c3d7f
Singupalli Kartik <skartik1706@gmail.com>
2024-03-10 16:37:25 +0530

    Fix sidebar link and add Sideee component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Router from './components/Router'
-import Profile from './components/Profile'
-import Sidebar from './components/sidebar'
-import News from './components/News'
-import FIndplayer from './components/FIndplayer'
-import UserData from './components/UserData'
-import HireTrainee from './components/HireTrainee'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Router from "./components/Router";
+import Profile from "./components/Profile";
+import Sidebar from "./components/sidebar";
+import News from "./components/News";
+import FIndplayer from "./components/FIndplayer";
+import UserData from "./components/UserData";
+import HireTrainee from "./components/HireTrainee";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-    <Router/>
-    <UserData></UserData>
-    {/* <HireTrainee></HireTrainee> */}
- </>
-  )
+      <Router />
+      <UserData></UserData>
+      {/* <HireTrainee></HireTrainee> */}
+    </>
+  );
 }
 
-export default App
+export default App;
